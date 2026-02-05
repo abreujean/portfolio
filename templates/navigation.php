83e01b3f2bbafa8d5f6c0b049c804ec1a0209a93
abreujean <jeandcabreu@gmail.com>
2026-02-05 15:31:36 -0300

    Feat: correções de bug

--- templates/navigation.php
+++ templates/navigation.php
@@ -24,25 +24,3 @@ if (!defined('ABSPATH')) {
         <?php get_template_part('templates/navigation-mobile'); ?>
     </div>
 </header>
-
-<?php
-/**
- * Fallback callback for primary menu
- */
-function portfolio_primary_menu_fallback() {
-    if (current_user_can('edit_theme_options')) {
-        ?>
-        <ul id="primary-menu-list" class="primary-menu-list">
-            <li class="menu-notice">
-                <?php
-                /* translators: %s: link to menus admin page */
-                printf(
-                    esc_html__('Please assign a menu to the Primary Menu location in %s.', 'portfolio'),
-                    '<a href="' . esc_url(admin_url('nav-menus.php')) . '">' . esc_html__('Appearance > Menus', 'portfolio') . '</a>'
-                );
-                ?>
-            </li>
-        </ul>
-        <?php
-    }
-}
